e212a30fd5fd057f2eaf4206f4d2e65fc910b2ea
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2025-10-07 16:26:32 +0800

    Rewrote how I thought about the menu - put it in an object list. AG

--- menu.py
+++ menu.py
@@ -5,29 +5,30 @@ from classfile import *
 
 data = []
 
+
 service_file = open("services.csv", "r")
 reader = csv.reader(service_file)
 rows = [row for row in reader if row]
 
 i=0
+services = []
 
 while i<len(rows):
     if len(rows[i]) == 0:
         i+=1
         continue
 
-    name= rows[i][0].strip()
+    
+    name = rows[i][0].strip()
     units = rows[i][1].strip()
-
-    tiers =[float(x) for x in rows[i+1]]
-
+    tiers = [float(x) for x in rows[i+1]]
     cost = [float(x) for x in rows[i+2]]
 
+    # create Service object and add it to the list
+    service = Service(name, units, tiers, cost, 0)
+    services.append(service)
 
-    i+=3
-
-    data.append(Service(name, units, tiers, cost))
-
+    i += 3  
 
 
 
@@ -38,12 +39,17 @@ print("To get rid of a subscription, press 2")
 print("Press s for the total breakdown of all subscriptions")
 print("Press Q to quit")
 
+subscriptions =[]
+
 answer = input("Please select your options")
 while answer !="Q":
     if answer == "1":
         sub_type = input("What subscription would you like?")
+        amount = input("Please enter the amount required")
         if sub_type == "AI":
             pass
+            
+    
 
         elif sub_type == "Computing":
             pass
