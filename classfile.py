15b66b317362dbfd40bfd3ae5ed8b6b47547b75e
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2025-10-08 12:59:44 +0800

    Added a function to add a subscription to the manager, AG

--- classfile.py
+++ classfile.py
@@ -4,6 +4,10 @@ class SubscriptionManager():
         self.subscriptions = []
     def overall_cost(self):
         return sum(s.cost for s in self.subscriptions)
+    
+    def add_subscription(self, type):
+        self.subscriptions.append(type)
+        print("Subscription added successfully :)")
 
 
 class Service():
@@ -21,6 +25,8 @@ class Service():
                 cost=self.cost[i]
                 print("The cost per unit for this amount is", self.cost[i])
                 print("The total cost is", cost * float(amount))
+
+
 
 
 
