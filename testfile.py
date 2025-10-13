20129f1e912bcd2fe106e43d39cd6b2fc7ad5024
duckducksillygoose <Ashleigh.Gurr@student.curtin.edu.au>
2025-10-13 13:58:32 +0800

    Adding some headings and notes. AG

--- testfile.py
+++ testfile.py
@@ -3,8 +3,17 @@ from menu import *
 
 manager= SubscriptionManager()
 
+###we are testing add subscription, delete subscription and get_price
+
 netflix = Service("Netflix", "per month", [1, 4, 7], [28, 18, 10])
 spotify = Service("Spotify", "per month", [10, 40, 100], [12.0, 7.0])
 youtube = Service("Youtube", "per month", [1, 2, 3], [19, 16, 13])
 
 services = [netflix, spotify, youtube]
+
+
+print("-----ADDING SUBSCRIPTIONS-------")
+
+
+
+print("----DELETING SUBSCRIPTIONS--------")
